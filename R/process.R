a0b13d37309b800c41b8794e3b1d562d88421f2d
Michael Pereira <monkeycycle@gmail.com>
2021-07-05 09:33:13 -0500

    Misc cleaning.

--- R/process.R
+++ R/process.R
@@ -6,6 +6,6 @@ source(dir_src("process_phac.R"))
 source(dir_src("process_vaccinations.R"))
 source(dir_src("process_variants.R"))
 source(dir_src("process_weekly_epi_report.R"))
-source(dir_src("process_covid19_districts.R"))
+# source(dir_src("process_covid19_districts.R"))
 
 
