da66fdb1c0370847a742381f825425e9da74f231
Michael Pereira <monkeycycle@gmail.com>
2022-01-07 14:24:08 -0600

    Limits

--- R/visualize_new_daily_tests_avg.R
+++ R/visualize_new_daily_tests_avg.R
@@ -12,7 +12,7 @@ p_new_daily_tests_avg <- plot_bar_timeseries_w_avg(
   title_str="Daily tests for COVID-19 reported in Manitoba",
   subtitle_str="", x_str="", y_str="",
   xmin="2020-03-01", xmax=xmax_var, xformat="%b", x_units="1 month",
-  ymin=0, ymax=8000, y_units="",
+  ymin=0, ymax=10000, y_units="",
   source_str="Manitoba Health", lastupdate_str=last_update_timestamp
 )
 
